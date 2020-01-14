33e33a4c032b5274a238d3cb26931a8175a88341
matchashiba <recr38@gmail.com>
2020-01-14 01:41:04 +0000

    Created new App component Created Header component Created ProductListItem component Created ProductList component Defined method in ProductList named getProducts to make a get call to server Defined componentDidMount method to handle the page after it has initially loaded Began initial styling with Bootstrap4 and CSS

---
+++ client/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="navbar bg-dark topBar">
+        <h4><i className="fas fa-dollar-sign"></i> Wicked Sales</h4>
+      </div>
+    );
+  }
+
+}
+
+export default Header;
---
+++ client/components/ProductList.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ProductListItem from './ProductListItem';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+    this.getProducts = this.getProducts.bind(this);
+    this.displayProducts = this.displayProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProducts();
+
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ products: data });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  displayProducts() {
+    const productList = [...this.state.products];
+    const products = productList.map((item, index) => {
+      return <ProductListItem key={index} image={item.image} productName={item.name} price={item.price} description={item.shortDescription} />;
+    });
+    return products;
+  }
+
+  render() {
+    const products = this.displayProducts();
+    return (
+      <div className="container">
+        <div className="row align-items-center">
+          {products}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
