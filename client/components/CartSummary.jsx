9308a9bd7cd1987a5afff11334b8962412df3fb3
matchashiba <recr38@gmail.com>
2020-01-16 20:40:54 +0000

    Created CartSummaryItem component to display each item's info Created CartSummary component to display all cart items to usert Added conditional to change view to cart Created onClick functionality for shopping cart Continued to add styling via Bootstap4

---
+++ client/components/CartSummary.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import CartSummaryItem from './CartSummaryItem';
+
+function CartSummary(props) {
+  const cartItems = props.cartItems;
+  const cartItemElements = cartItems.map(item => {
+    return <CartSummaryItem key={item.productId} image={item.image} productName={item.name} price={item.price} description={item.shortDescription} />;
+  });
+  let totalPrice = 0;
+  for (let i = 0; i < cartItems.length; i++) {
+    totalPrice += cartItems[i].price;
+  }
+  if (cartItemElements.length === 0) {
+    return (
+      <div>
+        <p className="text-muted" onClick={() => props.setView('catalog', {})}>{'< Back to catalog'}</p>
+        <h4>There are no items in your cart</h4>
+      </div>);
+  }
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <p className="text-muted" onClick={() => props.setView('catalog', {})}>{'< Back to catalog'}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h4>My Cart</h4>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          {cartItemElements}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h4>Item Total $ {(totalPrice / 100).toFixed(2)}</h4>
+        </div>
+      </div>
+    </div>);
+}
+
+export default CartSummary;
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
+    const cartParams = this.props.cart;
     return (
       <div className="navbar bg-dark topBar">
         <h4><i className="fas fa-dollar-sign"></i> Wicked Sales</h4>
-        <span className="shoppingCart float-right">{this.props.quantity} {this.props.item} <i className="fas fa-shopping-cart"/></span>
+        <span className="shoppingCart float-right">{this.props.quantity} {this.props.item} <i onClick={() => this.props.setView('cart', cartParams)} className="fas fa-shopping-cart"/></span>
       </div>
     );
   }
